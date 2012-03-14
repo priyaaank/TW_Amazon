4be0e7f70c1f204ef96d590498228ae12f7a545a
Sanjeewani Wijekoon <sanjeewani.wijekoon@gmail.com>
2012-03-14 15:08:49 +1100

    fix folder structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TWAmazon::Application.config.secret_token = '31499dba4f0498c7e51c28b75400d3d66a281543bc4492d91d20c3e033e8acd9793fd01b4105ad85b57a625debb23ad6cf05242637702e903c48e5268fb774e1'
