fa9ff075b39a3540384a8337f35c97018e53bf8b
MaiKLam <mai.lam@lamkhanhmai.com>
2012-03-21 15:07:11 +1100

    route index page to silent_auctions listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TWAmazon::Application.routes.draw do
+
+  match '/' => 'silent_auctions#index'
+
   resources :silent_auctions
 
   devise_for :users, :controllers => { :omniauth_callbacks => "sessions"}
