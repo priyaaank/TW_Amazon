5d881b5d86515e5cdb0e9bb927c083ea01443c30
Sanjeewani Wijekoon <sanjeewani.wijekoon@gmail.com>
2012-03-23 17:26:34 +1100

    changes made

--- features/support/components/silent_auction_component.rb
+++ features/support/components/silent_auction_component.rb
@@ -2,6 +2,10 @@ module SilentAuctionModule
   def create_silent_auction
     add :silent_auctions, SilentAuction.make!
   end
+  
+  def create_silent_auction_from_hash hash
+    add :silent_auctions, SilentAuction.make!(hash)
+  end
 
   def verify_silent_auction_has_title silent_auction
     silent_auction.title.blank?.should == false
