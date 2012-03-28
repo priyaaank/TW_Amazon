b776380d35eeb33f2fee35ceb7177ab6013ad854
Sanjeewani Wijekoon <sanjeewani.wijekoon@gmail.com>
2012-03-28 16:55:09 +1100

    modified feature files

---
+++ features/step_definitions/find_running_auction_steps.rb
@@ -0,0 +1,37 @@
+When /^the auction is open$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the auction is running$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the auction is closed$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the auction is not running$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^there are valid auctions with the following:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I view all auctions$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I can see the following auctions:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^there are no valid auctions$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I am told that no auctions are currently going$/ do
+  pending # express the regexp above with the code you wish you had
+end
