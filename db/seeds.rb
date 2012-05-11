be8cf5bb76bafd31e66a80fa7e4a3ff244e47971
Mai K Lam <mai.lam@lamkhanhmai.com>
2012-05-11 10:24:31 +1000

    add condition to seed db only if in test mode

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+if Rails.application.config.test_mode?
+  puts 'SETTING UP TEST ADMIN LOGIN'
 
-puts 'SETTING UP TEST ADMIN LOGIN'
+  @index = 1
 
-@index = 1
-
-2.times do
-  user = User.new(:username => "Admin_tw_#{@index}", :admin => true, :email=> nil, :password => 'adminpass', :password_confirmation => 'adminpass')
-  if(user.save!)
-    puts 'New admin created: ' << user.username
-    @index += 1
+  2.times do
+    user = User.new(:username => "Admin_tw_#{@index}", :admin => true, :email=> nil, :password => 'adminpass', :password_confirmation => 'adminpass')
+    if(user.save!)
+      puts 'New admin created: ' << user.username
+      @index += 1
+    end
   end
-end
 
-puts 'SETTING UP TEST USER LOGIN'
+  puts 'SETTING UP TEST USER LOGIN'
 
-@index = 1
+  @index = 1
 
-10.times do
-  user = User.new(:username => "User_tw_#{@index}", :admin => false, :email=> nil, :password => 'userpass', :password_confirmation => 'userpass')
-  if(user.save!)
-    puts 'New user created: ' << user.username
-    @index += 1
+  10.times do
+    user = User.new(:username => "User_tw_#{@index}", :admin => false, :email=> nil, :password => 'userpass', :password_confirmation => 'userpass')
+    if(user.save!)
+      puts 'New user created: ' << user.username
+      @index += 1
+    end
   end
 end
 
