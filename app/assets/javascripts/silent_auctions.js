eb466951eb160349572218f70d6d3b064e7c7c0f
Mai K Lam <mai.lam@lamkhanhmai.com>
2012-05-17 16:22:23 +1000

    add word count

--- app/assets/javascripts/silent_auctions.js
+++ app/assets/javascripts/silent_auctions.js
@@ -2,6 +2,13 @@ $(document).ready( function() {
     // to open auction photos in light box
     $('a.gallery').colorbox();
 
+    //have dynamic character count for description
+    $("#description").charCount({
+        allowed: 500,
+        warning: 20,
+        counterText: 'Characters left: '
+    });
+
     $(".bid_form").live("ajax:complete", function(event,xhr,status){
         $('#bid_amount').val('');
         $(this).removeAttr('disabled');
