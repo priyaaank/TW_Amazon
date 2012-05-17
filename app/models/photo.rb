593487d96100a943102e8dad4f86430497bdde57
Mai K Lam <mai.lam@lamkhanhmai.com>
2012-05-17 16:21:50 +1000

    basic file upload

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,18 @@
 class Photo < ActiveRecord::Base
   belongs_to :silent_auction
-  attr_accessible :image, :remote_image_url
+  attr_accessible :image, :remote_image_url, :image_cache, :remove_image
 
   mount_uploader :image, PhotoUploader
+
+  #one convenient method to pass jq_upload the necessary information
+  def to_jq_upload
+    {
+        "name" => read_attribute(:image),
+        "size" => image.size,
+        "url" => image.url,
+        "thumbnail_url" => image.thumb.url,
+        "delete_url" => picture_path(:id => id),
+        "delete_type" => "DELETE"
+    }
+  end
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -29,7 +29,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fit => [800, 600]
   #
   # def scale(width, height)
   #   # do something
