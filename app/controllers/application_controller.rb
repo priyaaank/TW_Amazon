597a13a2c5791f1718b43a3f4e74ad96f8f01d2d
Mai K Lam <mai.lam@lamkhanhmai.com>
2012-05-20 22:41:26 +1000

    add error page for record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :show_not_found_record_errors
+
+  def show_not_found_record_errors
+    @error_heading = "404 - Record Not Found"
+    @error_msg = "The auction you specified does not exist. Please check your provided auction id or your URL"
+    render_page
+  end
+
+  private
+  def render_page
+    respond_to do |format|
+      format.html {render 'show_errors'}
+    end
+  end
 end
