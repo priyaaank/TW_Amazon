38a99b4298749f24d2e0698678d5d2abc92e3701
MaiKLam <mai.lam@lamkhanhmai.com>
2012-05-23 10:57:28 +1000

    attempt to fix issue of cached img not display

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -30,11 +30,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
     end
   end
 
-  # cached file for Heroku deploy
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
