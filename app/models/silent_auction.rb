19f39994100325e222bf7bbd7225d0387a05dd4d
MaiKLam <mai.lam@lamkhanhmai.com>
2012-05-23 10:24:04 +1000

    fix query for closed auctions

--- app/models/silent_auction.rb
+++ app/models/silent_auction.rb
@@ -19,7 +19,7 @@ class SilentAuction < ActiveRecord::Base
                         :format => { :with => /^\d+?(?:\.\d{0,2})?$/, :message => "can only have 2 decimal places" }
 
   scope :running, where(:open => true)
-  scope :closed, joins(:bids).where(:open => false).group(:silent_auction_id)
+  scope :closed, includes(:bids).where("bids.id IS NOT NULL AND open = ?", false)
   scope :expired, includes(:bids).where("bids.id IS NULL AND open = ?", false)
 
   scope :recent, order('"silent_auctions"."created_at" desc')
