54ce5ec03dd74fb893c14655a20371a26c8bce72
paryanto <paryanto@thoughtworks.com>
2012-09-12 01:04:37 +1000

    add quotes to sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ TWAmazon::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = "sendmail"
   
   config.action_mailer.smtp_settings = {
     :address        => "smtp.gmail.com",
