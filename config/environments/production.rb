a45359bb88e80b525c88162b3231e1bf6d82e00f
paryanto <paryanto@thoughtworks.com>
2012-09-12 02:07:32 +1000

    remove www from thoughtworks.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ TWAmazon::Application.configure do
     :address        => "smtp.gmail.com",
     :port           => 587,
     #:domain         => "twgs.herokuapp.com",
-    :domain         => "www.thoughtworks.com",#gmail.com",
+    :domain         => "thoughtworks.com",#gmail.com",
     :user_name      => "twamazon@thoughtworks.com",#twgs.twgs@gmail.com",
     :password       => "TWp@55word!",#twgs.twgs",
     :authentication => "plain",
