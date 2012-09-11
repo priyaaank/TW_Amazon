55fab9d24cdace480c17407c445673a98aee0af8
paryanto <paryanto@thoughtworks.com>
2012-09-11 17:46:08 +1000

    reassigning email origin to twamazon

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => "smtp.gmail.com",
   :port           => "587",
   :domain         => "twgs.herokuapp.com",
-  :user_name      => "peter.aryanto@gmail.com",
-  :password       => "Hibik180",
+  :user_name      => "twamazon@thoughtworks.com",
+  :password       => "TWp@55word!",
   :authentication => "plain",
   :enable_startttls_auto => true
 }
