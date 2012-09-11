d7f514e7153b358b763201f7c8d92ad6658834e0
paryanto <paryanto@thoughtworks.com>
2012-09-12 02:04:55 +1000

    more attempt by set up the <setup_mail.rb> file

--- config/environments/setup_mail.rb
+++ config/environments/setup_mail.rb
@@ -2,9 +2,9 @@ ActionMailer::Base.sendmail_settings = {
   :address        => "smtp.gmail.com",
   :port           => "587",
   #:domain         => "twgs.herokuapp.com",
-  #:domain         => "gmail.com",
-  :user_name      => "twamazon@thoughtworks.com",
-  :password       => "TWp@55word!",
-  :authentication => :plain,
+  :domain         => "www.thoughtworks.com",#gmail.com",
+  :user_name      => "twamazon@thoughtworks.com",#twgs.twgs@gmail.com",
+  :password       => "TWp@55word!",#twgs.twgs",
+  :authentication => "plain",
   :enable_startttls_auto => true
 }
