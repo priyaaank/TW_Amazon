cc480a4bd0fa20cf0cff8dd6fa9bc2d550d51d02
paryanto <paryanto@thoughtworks.com>
2012-09-12 10:22:05 +1000

    go back to twgs.twgs@gmail.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,9 +26,9 @@ TWAmazon::Application.configure do
     :address        => "smtp.gmail.com",
     :port           => "587",
     #:domain         => "twgs.herokuapp.com",
-    :domain         => "mail.thoughtworks.com",#gmail.com",
-    :user_name      => "twamazon",#twgs.twgs@gmail.com",#,
-    :password       => "TWp@55word!",#twgs.twgs",#,
+    :domain         => "gmail.com",
+    :user_name      => "twgs.twgs@gmail.com",#twamazon@thoughtworks.com",
+    :password       => "twgs.twgs",#TWp@55word!",
     :authentication => :plain,
     :enable_startttls_auto => true
   }
