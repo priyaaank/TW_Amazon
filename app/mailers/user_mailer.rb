cf67fb43f93a639d77fb4223b534db105e0544b7
paryanto <paryanto@thoughtworks.com>
2012-10-25 14:58:12 +1100

    push to live (peter/oscar)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ActionMailer::Base
     @winner_id = winner_id
     @winner_amount = winner_amount
     @recipients =  "#{@winner_id}"
-    mail(:to => "#{@recipients}", :subject => "[TW Garage Sale] [SPAM] [Testing ONLY] Auction results for \"#{@title}\"", :from => 'GarageSale@no-reply.thoughtworks.com')
+    mail(:to => "#{@recipients}", :subject => "[TW Garage Sale] [SPAM] Auction results for \"#{@title}\"", :from => 'GarageSale@no-reply.thoughtworks.com')
   end
   
   def administrator_notification_close(title,bids_count,winner_id,winner_amount,alladmins,creator)
@@ -18,7 +18,7 @@ class UserMailer < ActionMailer::Base
     @winner_amount = winner_amount
     @recipients = "#{@creator}@thoughtworks.com"
     #@recipients = "twgs.twgs@gmail.com"
-    mail(:to => "#{@recipients}", :subject => "[TW Garage Sale] [SPAM] [Testing ONLY] Auction results for \"#{@title}\"", :from => 'GarageSale@no-reply.thoughtworks.com')    
+    mail(:to => "#{@recipients}", :subject => "[TW Garage Sale] [SPAM] Auction results for \"#{@title}\"", :from => 'GarageSale@no-reply.thoughtworks.com')    
   end
 
   def administrator_notification_expired(title,alladmins,creator)
@@ -26,29 +26,29 @@ class UserMailer < ActionMailer::Base
     @title = title
     @recipients = "#{@creator}@thoughtworks.com"
     #@recipients = "twgs.twgs@gmail.com"
-    mail(:to => "#{@recipients}", :subject => "[TW Garage Sale] [SPAM] [Testing ONLY] Auction for \"#{@title}\" has expired", :from => 'GarageSale@no-reply.thoughtworks.com')
+    mail(:to => "#{@recipients}", :subject => "[TW Garage Sale] [SPAM] Auction for \"#{@title}\" has expired", :from => 'GarageSale@no-reply.thoughtworks.com')
   end
   
   def send_announcement_to_other_users(auction)
     @title = auction.title
     @start_date = auction.start_date.strftime("%d %B %Y")
     @end_date = auction.end_date.strftime("%d %B %Y")
-    #@other_users = User.where("username <> ? AND region = ?", auction.creator, auction.region)
+    @other_users = User.where("username <> ? AND region = ?", auction.creator, auction.region)
     @all_recipients = "twgs.twgs@gmail.com"
-    # @other_users.each do |user|
-      # if @all_recipients != "" then 
-        # @all_recipients = @all_recipients + ", "
-      # end
-      # if user.email == nil
-        # @all_recipients = @all_recipients + user.username + "@thoughtworks.com"
-        # else if user.email == 'on'
-          # @all_recipients = @all_recipients + user.username + "@thoughtworks.com"
-        # end                  
-      # end
-    # end
+    @other_users.each do |user|
+      if @all_recipients != "" then 
+        @all_recipients = @all_recipients + ", "
+      end
+      if user.email == nil
+        @all_recipients = @all_recipients + user.username + "@thoughtworks.com"
+        else if user.email == 'on'
+          @all_recipients = @all_recipients + user.username + "@thoughtworks.com"
+        end                  
+      end
+    end
 
     # send the email only when there is at least 1 recipient
-    mail(:to => "#{@all_recipients}", :subject => "[TW Garage Sale] [SPAM] [Testing ONLY] New auction for \"#{@title}\"", :from => 'GarageSale@no-reply.thoughtworks.com')      
+    mail(:to => "#{@all_recipients}", :subject => "[TW Garage Sale] [SPAM] New auction for \"#{@title}\"", :from => 'GarageSale@no-reply.thoughtworks.com')      
   end
   
   def seller_notification_quick_sale_almost_ends(title,creator)
@@ -56,6 +56,6 @@ class UserMailer < ActionMailer::Base
     @title = title
     @recipients = "#{@creator}@thoughtworks.com"
     #@recipients = "twgs.twgs@gmail.com"
-    mail(:to => "#{@recipients}", :subject => "[TW Garage Sale] [SPAM] [Testing ONLY] Your sale of item \"#{@title}\" is about to end", :from => 'GarageSale@no-reply.thoughtworks.com')
+    mail(:to => "#{@recipients}", :subject => "[TW Garage Sale] [SPAM] Your sale of item \"#{@title}\" is about to end", :from => 'GarageSale@no-reply.thoughtworks.com')
   end
 end
