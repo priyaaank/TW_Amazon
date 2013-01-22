ed18d29b5b3024ff522145a43a4eb63ad8cb8905
Venkatesh Nannan <nvenky@gmail.com>
2013-01-22 23:20:45 +1100

    Venky: Fixed the bug related to displaying flag in region

--- app/assets/javascripts/flags.js
+++ app/assets/javascripts/flags.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
 
 	// turn select into dl
 	createDropDown();
-	
+
 	var $dropTrigger = $(".dropdown dt a");
 	var $languageList = $(".dropdown dd ul");
-	
+
 	// open and close list when button is clicked
 	$dropTrigger.toggle(function() {
 		$languageList.slideDown(200);
@@ -36,7 +36,7 @@ $(document).ready(function() {
 		var secondCombo = document.getElementById ("user_region");
         	secondCombo.value = clickedTitle;
         	document.forms[0].submit();
-		
+
 	});
 });
 
@@ -49,10 +49,10 @@ $(document).ready(function() {
 		var selected = source.find("option:selected");
 		var options = $("option", source);
 		$("#countryselect").append('<dl id="target" class="dropdown"></dl>')
-		$("#target").append('<dt class="' + selected.val() + '"><a href="#"><span class="flag"></span><em>' + selected.text() + '</em></a></dt>')
+		$("#target").append('<dt class="' + selected.text() + '"><a href="#"><span class="flag"></span><em>' + selected.text() + '</em></a></dt>')
 		$("#target").append('<dd><ul></ul></dd>')
 		options.each(function(){
-			$("#target dd ul").append('<li class="' + $(this).val() + '"><a href="#"><span class="flag"></span><em>' + $(this).text() + '</em></li>');
+			$("#target dd ul").append('<li class="' + $(this).text() + '"><a href="#"><span class="flag"></span><em>' + $(this).text() + '</em></li>');
 			});
-			
+
 	}
